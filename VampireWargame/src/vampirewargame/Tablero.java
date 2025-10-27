0bae1171a288f890c6521d42c338db74f91fd528
Jiny-perez <jinyperez55@gmail.com>
2025-10-26 21:21:17 -0600

    Tablero ya realizado

--- VampireWargame/src/vampirewargame/Tablero.java
+++ VampireWargame/src/vampirewargame/Tablero.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vampirewargame;
+
+import javax.swing.*;
 
 /**
  *
  * @author marye
  */
-public class Tablero {
+class Tablero {
+
+    private Piezas[][] tablero = new Piezas[6][6];
+    private ImageIcon[][] piezas = new ImageIcon[6][6];
+
+    public void PosicionPiezas() {
+        ImageIcon imagenHombreLobo = new ImageIcon(getClass().getResource("/Imagenes/HombreLobo.png"));
+        ImageIcon imagenVampiro = new ImageIcon(getClass().getResource("/Imagenes/Vampiro.png"));
+        ImageIcon imagenMuerte = new ImageIcon(getClass().getResource("/Imagenes/Muerte.png"));
+        
+        piezas[0][0] = imagenHombreLobo;
+        piezas[0][1] = imagenVampiro;
+        piezas[0][2] = imagenMuerte;
+        piezas[0][3] = imagenMuerte;
+        piezas[0][4] = imagenVampiro;
+        piezas[0][5] = imagenHombreLobo;
+
+        piezas[5][0] = imagenHombreLobo;
+        piezas[5][1] = imagenVampiro;
+        piezas[5][2] = imagenMuerte;
+        piezas[5][3] = imagenMuerte;
+        piezas[5][4] = imagenVampiro;
+        piezas[5][5] = imagenHombreLobo;
+    }
     
+    public ImageIcon getPiezas(int fila, int col) {
+        return piezas[fila][col];
+    }
+    public Piezas getTablero(int fila, int col) {
+        return tablero[fila][col];
+    }
+
+    public void setPieza(int fila, int col, Piezas p) {
+        tablero[fila][col] = p;
+    }
+
+    public boolean casillaVacia(int fila, int col) {
+        if (tablero[fila][col] == null) {
+            return true;
+        }
+        return false;
+    }
 }
+
