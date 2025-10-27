e9ba7369c4ae5b6df1d9b871391f677fdc674ae1
Jiny-perez <jinyperez55@gmail.com>
2025-10-26 20:59:34 -0600

    Clase usernamePassword lista.

--- VampireWargame/src/MenuInicial/PasswordException.java
+++ VampireWargame/src/MenuInicial/PasswordException.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MenuInicial;
 
 /**
  *
  * @author marye
  */
-public class PasswordException {
-    
+public class PasswordException extends Exception{
+        public PasswordException(String mensaje){
+        super(mensaje);
+    }
 }
---
+++ VampireWargame/src/MenuInicial/UsernameException.java
@@ -0,0 +1,11 @@
+package MenuInicial;
+
+/**
+ *
+ * @author marye
+ */
+public class UsernameException extends Exception {
+    public UsernameException(String mensaje){
+        super(mensaje);
+    }
+}
