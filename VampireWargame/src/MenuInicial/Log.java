3aec4bd1da8df6df45c78d26a41334ce1800c931
Jiny-perez <jinyperez55@gmail.com>
2025-10-26 21:05:32 -0600

    GUI del botono Log In

--- VampireWargame/src/MenuInicial/Log.java
+++ VampireWargame/src/MenuInicial/Log.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MenuInicial;
+
+import java.util.Calendar;
 
 /**
  *
  * @author marye
  */
 public class Log {
+
+    private String player;
+    private String descripcion;
+    private Calendar fecha;
+
+    public Log(String player, String descripcion) {
+        this.player = player;
+        this.descripcion = descripcion;
+        this.fecha=fecha;
+
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public Calendar getFecha(){
+        return fecha;
+    }
+    
+    public String mostrarFecha() {
+        return fecha.get(Calendar.DAY_OF_MONTH) + "/" +
+               (fecha.get(Calendar.MONTH)+1) + "/" +
+               fecha.get(Calendar.YEAR) + " " +
+               fecha.get(Calendar.HOUR_OF_DAY) + ":" +
+               fecha.get(Calendar.MINUTE);
+    }
     
 }
+
